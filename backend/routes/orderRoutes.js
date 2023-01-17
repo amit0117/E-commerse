e0d308454b6008217cbf41323f3359f56d5af13b
Amit <20je0117@cve.iitism.ac.in>
2023-01-17 09:59:22 +0530

    Implement PayPal payment method

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express'
-import { addOrderItems ,getOrderById,updateOrderToPaid} from '../controller/orderController.js'
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controller/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
-router.post('/',protect,addOrderItems)
-router.get('/:id',protect,getOrderById)
-router.put('/:id/pay',protect,updateOrderToPaid)
+router.post('/', protect, addOrderItems)
+router.get('/myorders', protect, getMyOrders)
+router.get('/:id', protect, getOrderById)
+router.put('/:id/pay', protect, updateOrderToPaid)
 export default router
