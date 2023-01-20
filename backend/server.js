00f09d1077e00495bb3a8022216b56f3cb461317
Amit <20je0117@cve.iitism.ac.in>
2023-01-20 23:35:18 +0530

    make change in sever file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import orderRoutes from './routes/orderRoutes.js'
 import { notFound,errorHandler } from './middleware/errorMiddleware.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import pkg from 'cloudinary'
-// import multer from 'multer'
+import multer from 'multer'
 import morgan from 'morgan'
 const cloudinary=pkg
 dotenv.config();
@@ -38,16 +38,16 @@ app.get('/api/config/paypal',(req,res)=>{
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
-// if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'./frontend/build')))
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
 
-    app.get('*',(req,res)=>res.join((__dirname,'./frontend/build/index.html')))
-// }else{
-    // app.get('/',(req,res)=>{
-    //     res.send("CheapShop is running ..")
-    // })
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+    app.get('/',(req,res)=>{
+        res.send("CheapShop is running ..")
+    })
     
-// }
+}
 
 app.use(notFound)
 app.use(errorHandler)
